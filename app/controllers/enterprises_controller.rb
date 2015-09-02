2ac287ffb75d40e1a57f4cbf4d83dd04ccdf5471
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-02 17:22:36 +0200

    compute amount of employees

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -1,9 +1,17 @@
-require './lib/tamayo/tamayocompute.rb'
+#require './lib/tamayo/tamayocompute.rb'
 
 class EnterprisesController < ApplicationController
   def show
     @enterprise = Enterprise.find(params[:id])
     @others = Enterprise.all
-    @gamesession = GameSession.find(3)
+    @gamesession = GameSession.find(@enterprise.game_session_id)
+    @employees = current_amount_of_employees
+  end
+
+  def current_amount_of_employees
+    gamesession_params = GameSession.find(3)
+    initial_amount_of_employees = gamesession_params.initial_number_of_employees
+    employee_variation = GameDecision.pluck(:employees_variation)
+    return employee_variation.reduce(:+) + initial_amount_of_employees
   end
 end
