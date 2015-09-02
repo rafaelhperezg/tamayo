f704826b78b6248fc5c774664050648b8352c684
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-02 18:29:05 +0200

    fies

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -6,12 +6,40 @@ class EnterprisesController < ApplicationController
     @others = Enterprise.all
     @gamesession = GameSession.find(@enterprise.game_session_id)
     @employees = current_amount_of_employees
+    @gamedecisions = GameDecision.where(enterprise_id: @enterprise)
+    raise
   end
 
-  def current_amount_of_employees
-    gamesession_params = GameSession.find(3)
-    initial_amount_of_employees = gamesession_params.initial_number_of_employees
-    employee_variation = GameDecision.pluck(:employees_variation)
-    return employee_variation.reduce(:+) + initial_amount_of_employees
+  def array_of_employee_variations
+    @gamedecisions.pluck(:employees_variation)
   end
+
+  # def array_of_contract_changes
+  #   @gamedecisions.pluck(:new_contract_id)
+  # end
+
+  def initial_number_of_employees
+    @gamesession.initial_number_of_employees
+  end
+
+  # def initial_amount_of_cash
+  #   @gamesession.initial_amount_of_cash
+  # end
+
+  # def current_amount_of_employees
+  #   array_of_employee_variations << initial_number_of_employees
+  # end
+
+  # def current_amount_of_cash
+  # end
 end
+
+
+# Calcul du cash disponible
+
+## employee_variation_costs = 0
+## if employee_variation > 0
+## => employee_variation_costs += hiring_cost
+## elsif employee_variation < 0
+## => employee_variation_costs -= firing_cost
+
