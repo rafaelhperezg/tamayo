50d011c6f2e41c921965a1eca4868f8fb93db189
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-02 14:19:37 +0200

    buttons and link of costs with game session

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -2,5 +2,6 @@ class EnterprisesController < ApplicationController
   def show
     @enterprise = Enterprise.find(params[:id])
     @others = Enterprise.all
+    @gamesession = GameSession.find(3)
   end
 end
