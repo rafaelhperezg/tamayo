ced2b2c793018fd9d82bb9efb22c829863183352
Rafael Perez <rapegoto@gmail.com>
2015-09-03 10:46:02 +0200

    global simple form for contract and employees decision ok

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -6,12 +6,14 @@ class EnterprisesController < ApplicationController
     @others = Enterprise.all
     @gamesession = GameSession.find(@enterprise.game_session_id)
     @employees = current_amount_of_employees
+    @game_decision = GameDecision.new
   end
 
   def current_amount_of_employees
-    gamesession_params = GameSession.find(3)
+    gamesession_params = GameSession.find(1)
     initial_amount_of_employees = gamesession_params.initial_number_of_employees
     employee_variation = GameDecision.pluck(:employees_variation)
-    return employee_variation.reduce(:+) + initial_amount_of_employees
+    employee_variation << initial_amount_of_employees
+    return employee_variation.reduce(:+)
   end
 end
