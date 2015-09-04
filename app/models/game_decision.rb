f681f876c7eb3840ccfb80e135ab3b159efb2877
Rafael Perez <rapegoto@gmail.com>
2015-09-04 15:18:52 +0200

    add validation=>impossible to fire more people thant real number of people

--- app/models/game_decision.rb
+++ app/models/game_decision.rb
@@ -2,4 +2,12 @@ class GameDecision < ActiveRecord::Base
   belongs_to :enterprise
   belongs_to :contract
   validates :employees_variation, :numericality => { :only_integer => true }
+
+  validate :minimum_employee_count
+
+  def minimum_employee_count
+    if enterprise.current_number_of_employees + employees_variation < 0
+      errors.add :employees_variation, "Employee count cannot be negative"
+    end
+  end
 end
