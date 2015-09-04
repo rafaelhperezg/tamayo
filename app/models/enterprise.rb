e54d7cce4ab46ca3cf3cc47ce593ef3885feb215
Rafael Perez <rapegoto@gmail.com>
2015-09-04 12:21:25 +0200

    modify name cost of raw materials

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -9,4 +9,11 @@ class Enterprise < ActiveRecord::Base
   def compute_salaries_cost
     current_number_of_employees * game_session.salary_per_employee
   end
+
+  # ------------RP: Costs calcul----------------------
+
+    def cost_of_raw_materials_for_today(current_day)
+      ORDERS[current_day] * gamesession.material_cost
+      # today_orders_received * cost_of_raw_materials_per_item
+    end
 end
