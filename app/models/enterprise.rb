ac3d71685fd4aab6c9291354fa8dba63757836d1
Rafael Perez <rapegoto@gmail.com>
2015-09-04 12:29:09 +0200

    Update cost of hiring/firing method

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -12,8 +12,21 @@ class Enterprise < ActiveRecord::Base
 
   # ------------RP: Costs calcul----------------------
 
-    def cost_of_raw_materials_for_today(current_day)
-      ORDERS[current_day] * gamesession.material_cost
-      # today_orders_received * cost_of_raw_materials_per_item
+  def cost_of_raw_materials_for_today(current_day)
+    ORDERS[current_day] * gamesession.material_cost
+    # today_orders_received * cost_of_raw_materials_per_item
+  end
+
+  def cost_of_hiring_and_firing_for_today(employee_variation_from_game_decisions_for_today)
+    if (employee_variation_from_game_decisions_for_today) > 0
+      game_session.hiring_cost * employee_variation_from_game_decisions_for_today
+    # (constant_employee_hiring_cost) * (employee_variation_from_game_decisions_for_today)
+    elsif (employee_variation_from_game_decisions_for_today) < 0
+      game_session.firing_cost * employee_variation_from_game_decisions_for_today
+    # (constant_employee_firing_cost) * (employee_variation_from_game_decisions_for_today)
+    else
+      0
     end
+  end
+
 end
