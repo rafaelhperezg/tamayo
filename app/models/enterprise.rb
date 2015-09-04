676aa6c7ac502391e4c27e57e81c5b95ff647ad6
Rafael Perez <rapegoto@gmail.com>
2015-09-04 12:41:58 +0200

    little modif on comments

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -10,7 +10,7 @@ class Enterprise < ActiveRecord::Base
     current_number_of_employees * game_session.salary_per_employee
   end
 
-  # ------------RP: Costs calcul----------------------
+  # ------------Costs calcul----------------------
 
   def cost_of_raw_materials_for_today(current_day)
     ORDERS[current_day] * gamesession.material_cost
