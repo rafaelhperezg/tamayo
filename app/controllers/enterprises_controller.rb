e1a3f6fe10aa936c58fc23e00bec35214c483937
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-07 18:41:33 +0200

    fixed formatting in enterprise controller

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -21,24 +21,24 @@ class EnterprisesController < ApplicationController
 
 
 # PRODUCTION VARIABLES
-    @today_orders_received              = ORDERS[@current_day - 1] #as current_day at start will be updated to 1, the -1 allow as to get ORDERS[0]
-    @current_number_of_employees        = @enterprise.current_number_of_employees
-    @today_workshop_production_capacity = @enterprise.today_workshop_production_capacity
-    @previous_backlog                   = @enterprise.current_backlog #should be here because in next line backlog will be recalculated when calling total to produce
-    @total_to_produce_today             = @enterprise.total_to_produce_today(@today_orders_received, @enterprise.current_backlog)
-    @products_manufactured_today        = @enterprise.products_manufactured_today(@today_workshop_production_capacity, @total_to_produce_today)
-    @when_can_todays_orders_be_delivered= @enterprise.when_can_todays_orders_be_delivered(@today_workshop_production_capacity, @total_to_produce_today)
+    @today_orders_received                = ORDERS[@current_day - 1] #as current_day at start will be updated to 1, the -1 allow as to get ORDERS[0]
+    @current_number_of_employees          = @enterprise.current_number_of_employees
+    @today_workshop_production_capacity   = @enterprise.today_workshop_production_capacity
+    @previous_backlog                     = @enterprise.current_backlog #should be here because in next line backlog will be recalculated when calling total to produce
+    @total_to_produce_today               = @enterprise.total_to_produce_today(@today_orders_received, @enterprise.current_backlog)
+    @products_manufactured_today          = @enterprise.products_manufactured_today(@today_workshop_production_capacity, @total_to_produce_today)
+    @when_can_todays_orders_be_delivered  = @enterprise.when_can_todays_orders_be_delivered(@today_workshop_production_capacity, @total_to_produce_today)
 
 # COSTS VARIABLES =>Test for the 4 variables: OK
-    @cost_of_salaries_for_today          = @enterprise.cost_of_salaries_for_today
-    @cost_of_raw_materials_for_today     = @enterprise.cost_of_raw_materials_for_today(@today_orders_received)
-    @cost_of_hiring_and_firing_for_today = @enterprise.cost_of_hiring_and_firing_for_today(@gamedecisions.last.employees_variation)
-    @total_money_spent_today             = @cost_of_salaries_for_today + @cost_of_raw_materials_for_today + @cost_of_hiring_and_firing_for_today
+    @cost_of_salaries_for_today           = @enterprise.cost_of_salaries_for_today
+    @cost_of_raw_materials_for_today      = @enterprise.cost_of_raw_materials_for_today(@today_orders_received)
+    @cost_of_hiring_and_firing_for_today  = @enterprise.cost_of_hiring_and_firing_for_today(@gamedecisions.last.employees_variation)
+    @total_money_spent_today              = @cost_of_salaries_for_today + @cost_of_raw_materials_for_today + @cost_of_hiring_and_firing_for_today
 
 # SALES VARIABLES =>Test for the 3 variables: OK
-    @current_contract_id                 = @previous_game_decision.new_contract_id
-    @contract                            = Contract.find(@current_contract_id)
-    @total_sales_for_today               = @enterprise.total_sales_for_today( @products_manufactured_today, @contract.timeframe, @when_can_todays_orders_be_delivered, @contract.price )
+    @current_contract_id                  = @previous_game_decision.new_contract_id
+    @contract                             = Contract.find(@current_contract_id)
+    @total_sales_for_today                = @enterprise.total_sales_for_today( @products_manufactured_today, @contract.timeframe, @when_can_todays_orders_be_delivered, @contract.price )
 
 # TREASURY VARIABLES =>Test for the 2 variables: OK
 
