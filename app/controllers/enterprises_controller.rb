57b21c257094a146d3a89de696c1e60901a17ced
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-09 11:44:20 +0200

    commit all the changes

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -8,23 +8,23 @@ class EnterprisesController < ApplicationController
   # todo gamedecisions, current_day
 
   def show
-    @all_enterprises = Enterprise.all
+    @all_enterprises        = Enterprise.all
     @all_enterprises_sorted = @all_enterprises.sort_by { |enterprise| enterprise.current_cash}.reverse
-    @gamedecision = GameDecision.new
+    @gamedecision           = GameDecision.new
     @previous_game_decision = GameDecision.where(enterprise_id: @enterprise).last
     # Next 2 lines will be needed when current_day will be managed by worker
     # @gamesession.current_day = @gamesession.update_current_day
     # @current_day = @gamesession.current_day
     # Next 2 lines will be DELETED when current_day will be managed by worker
     @enterprise.current_day = @enterprise.update_current_day
-    @current_day = @enterprise.current_day
+    @current_day            = @enterprise.current_day
 
 
 # PRODUCTION VARIABLES
     @today_orders_received              = ORDERS[@current_day -1] #as current_day at start will be updated to 1, the -1 allow as to get ORDERS[0]
     @current_number_of_employees        = @enterprise.current_number_of_employees
     @today_workshop_production_capacity = @enterprise.today_workshop_production_capacity
-    @previous_backlog                       = @enterprise.current_backlog #should be here because in next line backlog will be recalculated when calling total to produce
+    @previous_backlog                   = @enterprise.current_backlog #should be here because in next line backlog will be recalculated when calling total to produce
     @total_to_produce_today             = @enterprise.total_to_produce_today(@today_orders_received, @enterprise.current_backlog)
     @products_manufactured_today        = @enterprise.products_manufactured_today(@today_workshop_production_capacity, @total_to_produce_today)
     @when_can_todays_orders_be_delivered= @enterprise.when_can_todays_orders_be_delivered(@today_workshop_production_capacity, @total_to_produce_today)
@@ -50,15 +50,15 @@ class EnterprisesController < ApplicationController
 
   # ------Methods for before action---------
   def find_enterprise
-    @enterprise = Enterprise.find(params[:id])
+    @enterprise     = Enterprise.find(params[:id])
   end
 
   def find_game_session
-    @gamesession = GameSession.find(@enterprise.game_session_id)
+    @gamesession    = GameSession.find(@enterprise.game_session_id)
   end
 
   def get_game_decisions
-    @gamedecisions = GameDecision.where(enterprise_id: @enterprise)
+    @gamedecisions  = GameDecision.where(enterprise_id: @enterprise)
   end
   # /----------------------------------------
 end
