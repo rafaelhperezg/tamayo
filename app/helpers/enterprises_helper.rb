bf5896ee415e4e6bf471e1acf7eb70017b48f36a
Paul-Etienne Coisne <coisnepe@gmail.com>
2015-09-09 13:18:04 +0200

    added tooltips

--- app/helpers/enterprises_helper.rb
+++ app/helpers/enterprises_helper.rb
@@ -4,7 +4,19 @@ module EnterprisesHelper
   end
 
   def production_queue
+  end
 
+  def print_number_of_employees(number_of_employees)
+    if number_of_employees > 1
+      "You have #{number_of_employees} employees"
+    elsif number_of_employees == 1
+      "You have only 1 employee"
+    else
+      "You don't have any employees"
+    end
+  end
+
+  def print_hire_fire_costs
   end
 end
 
