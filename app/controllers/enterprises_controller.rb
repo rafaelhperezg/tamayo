511a024f520f7187788703a24b6898e1ac9a08fb
Rafael Perez <rapegoto@gmail.com>
2015-09-10 13:25:37 +0200

    Launch a modal when the game is end. Test ok but (with an existing modal that has other purpose) now we need a new modal adapted

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -14,9 +14,7 @@ class EnterprisesController < ApplicationController
 
     # Next 3 lines will be DELETED when current_day will be managed by worker
     @enterprise.current_day = @enterprise.update_current_day #=>Now this is made by RT
-    if @enterprise.current_day >= @gamesession.virtual_duration
-      flash.now[:error] = "Could not save client"
-    else
+    unless @enterprise.current_day == @gamesession.virtual_duration
       @enterprise.hyper_method
     end
     @current_day = @enterprise.current_day
